dfb29d547f307828fbc2fe942de5743a5626feef
ysk-hello <hello.ysk1905@gmail.com>
2023-11-17 22:23:39 +0900

    #1 ラジオ番組表

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -8,14 +8,27 @@ class ProgramsController < ApplicationController
     today = Date.today.strftime('%Y%m%d')
 
     @stations = Array.new
-    AvailableStation.where(prefecture_code: selected_prefecture_code).each do |record|
-      @stations.push(record.station)
-      pp record.station
+    AvailableStation.where(prefecture_code: selected_prefecture_code).each do |relation|
+      grouped = relation.station.programs.group_by{|prog| prog.start_time.strftime('%H')}
+      @stations.push(RadioStation.new(relation.station.name, grouped))
     end
+    @hours = ['05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+      '21', '22', '23', '00', '01', '02', '03', '04']
   end
 
   def show
     @program = Program.find(params[:id])
     @message = Message.new(session[:message])
   end
+end
+
+class RadioStation
+  attr_accessor :name
+  attr_accessor :progHash
+
+  def initialize(name, progHash)
+    @name = name
+    @progHash = progHash
+  end
+
 end

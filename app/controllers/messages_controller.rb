367f4eb3bb9d3116c7ca8d8cdd0dca30e10a3e11
ysk-hello <hello.ysk1905@gmail.com>
2023-12-01 21:55:33 +0900

    削除用パスワードが未入力でも保存できてしまう問題を修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,27 +2,20 @@ class MessagesController < ApplicationController
     def create
         message = Message.new(message_params)
         
-        # ハッシュ化
-        # https://qiita.com/kobayashimakoto/items/603d6434ba1952dfea69
-        message.delete_password = BCrypt::Password.create(message.delete_password)
-        
-        if message.save
+        if message.valid?
+            # ハッシュ化
+            # https://qiita.com/kobayashimakoto/items/603d6434ba1952dfea69
+            message.delete_password = BCrypt::Password.create(message.delete_password)
+            message.save
+
             session[:message] = nil
             flash[:info] = ["保存しました。"]
-            redirect_to "/programs/#{message["program_id"]}"
         else
-            # 失敗の場合、ルーティングせず、再描画
-            # 変数を用意しておく必要あり
-            # URLが変わってしまう
-            # @program = Program.find(message["program_id"])
-            # @message = message
-            # render "programs/show"
-
-            # https://qiita.com/yuyasat/items/49e3296f3c64fccc7811
             session[:message] = message.attributes.slice(*message_params.keys)
             flash[:danger] = message.errors.full_messages
-            redirect_to "/programs/#{message["program_id"]}"
         end
+
+        redirect_to "/programs/#{message["program_id"]}"
     end
 
     def destroy
